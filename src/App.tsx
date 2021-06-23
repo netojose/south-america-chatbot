06c24a12073f2ac2c0025aa4ba5f98afabf43d46
José Neto <sputinykster@gmail.com>
2021-06-23 03:47:17 +0300

    feat: implemented code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
-import Chat from './containers/Chat';
-import Dashboard from './containers/Dashboard';
+import PageLoader from './components/PageLoader';
 import store from './redux/store';
+
+// import Chat from './containers/Chat';
+// import Dashboard from './containers/Dashboard';
+
+const Chat = lazy(() => import('./containers/Chat'));
+const Dashboard = lazy(() => import('./containers/Dashboard'));
 
 const DASHBOARD_ROUTE = '/dashboard';
 const CHAT_ROUTE = '/chat/:userID';
@@ -12,11 +17,13 @@ const CHAT_ROUTE = '/chat/:userID';
 const App = (): React.ReactElement => (
   <Provider store={store}>
     <Router>
-      <Switch>
-        <Route path={DASHBOARD_ROUTE} component={Dashboard} />
-        <Route path={CHAT_ROUTE} component={Chat} />
-        <Redirect to={DASHBOARD_ROUTE} />
-      </Switch>
+      <Suspense fallback={<PageLoader />}>
+        <Switch>
+          <Route path={DASHBOARD_ROUTE} component={Dashboard} />
+          <Route path={CHAT_ROUTE} component={Chat} />
+          <Redirect to={DASHBOARD_ROUTE} />
+        </Switch>
+      </Suspense>
     </Router>
   </Provider>
 );
