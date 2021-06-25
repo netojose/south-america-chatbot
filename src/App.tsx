408750edb06b7c748cf0ce716b07654d7cba2899
José Neto <sputinykster@gmail.com>
2021-06-25 23:08:08 +0300

    fix: replaced react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,19 +14,21 @@ const DASHBOARD_ROUTE = '/dashboard';
 const CHAT_ROUTE = '/chat/:userID';
 
 const App = (): React.ReactElement => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Router>
-        <Suspense fallback={<PageLoader />}>
-          <Switch>
-            <Route path={DASHBOARD_ROUTE} component={Dashboard} />
-            <Route path={CHAT_ROUTE} component={Chat} />
-            <Redirect to={DASHBOARD_ROUTE} />
-          </Switch>
-        </Suspense>
-      </Router>
-    </PersistGate>
-  </Provider>
+  <HelmetProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Suspense fallback={<PageLoader />}>
+            <Switch>
+              <Route path={DASHBOARD_ROUTE} component={Dashboard} />
+              <Route path={CHAT_ROUTE} component={Chat} />
+              <Redirect to={DASHBOARD_ROUTE} />
+            </Switch>
+          </Suspense>
+        </Router>
+      </PersistGate>
+    </Provider>
+  </HelmetProvider>
 );
 
 export default App;
